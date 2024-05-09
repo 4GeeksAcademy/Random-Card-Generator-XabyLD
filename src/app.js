88d00636bcd49fd634e4f30bb3fecfc4ac8b6680
Longodios <tusaromix@gmail.com>
2024-05-09 16:31:45 +0200

    Carta creada, faltan estilos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let selectorHeaderSuit = document.querySelector(".card-header");
+let selectorBodyNumber = document.querySelector(".card-body");
+let selectorFooterSuit = document.querySelector(".card-footer");
+let suit;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const numberSuitRandom = () => {
+  let randomSuit = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 13 + 1);
+  // ♦ ♥ ♠ ♣
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  if (randomSuit === 0) {
+    suit = "♦";
+  } else if (randomSuit === 1) {
+    suit = "♥";
+  } else if (randomSuit === 2) {
+    suit = "♠";
+  } else {
+    suit = "♣";
+  }
+
+  if (randomNumber === 10) {
+    randomNumber = "T";
+  } else if (randomNumber === 11) {
+    randomNumber = "J";
+  } else if (randomNumber === 12) {
+    randomNumber = "Q";
+  } else if (randomNumber === 13) {
+    randomNumber = "K";
+  } else if (randomNumber === 1) {
+    randomNumber = "A";
+  }
+
+  selectorHeaderSuit.innerHTML = suit;
+  selectorFooterSuit.innerHTML = suit;
+  selectorBodyNumber.innerHTML = randomNumber;
+
+  if (suit === "♥") {
+    suit.style.backgroundColor = "red";
+  }
+};
+
+const colorSuit = () => {
+  console.log(suit);
+  if (selectorHeaderSuit === "♦") {
+    selectorHeaderSuit.innerHTML = suit;
+    suit.style.backgroundColor = "blue";
+  }
 };
